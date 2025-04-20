91425c86ec532ad747d0905218ee74c6f13cc8e3
cs-jimmy <jumanah.study@gmail.com>
2025-04-20 02:19:36 +0200

    Stores variables

--- Lp_solver.py
+++ Lp_solver.py
@@ -1,10 +1,24 @@
-
-Objective_function={}
+from tabulate import tabulate
+Objective={} #Dictionary for objective function variables eg: {'x':2,'y':7,'z':9}
 num_vars=int(input("Enter the number of variables: "))
 for i in range(num_vars):
-    var_name=input("Variable name: ")
+    var_name=input(f"Variable {i+1} name: ")
     coeff=float(input(f"coeffecient for {var_name}: "))
-    Objective_function[var_name]=coeff #maps coeff to the variable
+    Objective[var_name]=coeff #maps coeff to the variable
 obj_type=input("Enter optimization type (max/min): ")
-print(Objective_function)
+print(tabulate(Objective.items(), headers=["Variable", "Coefficient"], tablefmt="presto", colalign=("center", "center")))
+#print(Objective)
+
+Constraints=[] #List for constraint info
+num_const=int(input("Enter the number of constraints:"))
+for i in range(num_const):
+    print(f"\n======Constraint {i+1}======")
+    Constraint={}
+    for var in Objective:    # "Variable coefficients"
+        Constraint[var]=float(input(f"Coefficient for {var}:"))
+    Constraint["type"]=input("Choose constraint type (<=, >=, =) ")
+    Constraint["rhs"]=float(input("Enter Right-hand side vale: "))
+    Constraint["name"]=input("What is this constraint for? (e.g., time, cost, resources): ")
+    Constraints.append(Constraint)
+print(Constraints)
 
